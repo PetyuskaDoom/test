c4835bcf16974bfb655647e52bfc1d9c79747723
PetyuskaDoom <patzer1941@gmail.com>
2019-06-03 19:54:30 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+console.log('node running...');
+
+// var http = require('http')
+
+// onRequest(request, response) {
+//   response.writeHead(200, {
+//     'Content-Type': 'text/plain'});
+//   response.write('Hello Gabi');
+
+//   response.end();
+// }
+
+// http.createServer(onRequest).listen(8000)
+
+document.addEventListener('DOMContentLoaded', getWhile2);
+
+// shift alt down arrow - copy line down on next line
+// alt down arrow - move line down or up on next line
+function goWhile() {
+  console.log('In goWhile function...');
+}
+
+function getWhile2() {
+  console.log('In goWhile function...');
+}
